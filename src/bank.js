f6438dcd1aa62b2c1fd30da675271d4530f7ae65
ibrahimasy11 <ibhmsy11@gmail.com>
2025-11-24 15:15:49 -0500

    Complete Assessment

--- src/bank.js
+++ src/bank.js
@@ -1,29 +1,40 @@
 class BankAccount {
   #balance = 0;
+
+  static #totalNumberOfAccounts = 0; // i added that
+
   constructor(accountNumber, ownerName) {
     this.accountNumber = accountNumber;
     this.ownerName = ownerName;
+
     BankAccount.#totalNumberOfAccounts++;
   }
 
   deposit(amount) {
-    this.balance += amount;
-    console.log(`Deposited $${amount}. New balance: $${this.balance}`);
-    return this.balance;
+    this.#balance += amount;
+    console.log(`Deposited $${amount}. New balance: $${this.#balance}`);
+    return this.#balance;
+
+    // missing multiple #. place them in balance
   }
 
   withdraw(amount) {
-    if (amount > this.balance) {
+    if (amount > this.#balance) {
       console.log(`Withdrawal failed. Insufficient funds.`);
     } else {
-      this.balance -= amount;
-      console.log(`Withdrew $${amount}. New balance: $${this.balance}`);
+      this.#balance -= amount;
+      console.log(`Withdrew $${amount}. New balance: $${this.#balance}`);
     }
-    return this.balance;
+    return this.#balance;
+
+    // missing multiple #. place them in balance
   }
 
   getBalance() {
-    return #balance;
+    console.log(`Your account balance is: $${this.#balance}`);
+    return this.#balance;
+
+    // missing the 'this' keyword. but not printing the balance
   }
 
   static getTotalNumberOfAccounts() {
@@ -33,29 +44,33 @@ class BankAccount {
 
 class Bank {
   accounts = [];
+
   constructor(name) {
     this.name = name;
   }
 
   addAccount(account) {
-    accounts.push(account);
+    this.accounts.push(account);
+    // missing the this keyword
   }
 
   getTotalBalance() {
     let total = 0;
     this.accounts.forEach((account) => {
-      total += account.balance;
+      total += account.getBalance();
     });
     return total;
   }
 
   findAccount(accountNumber) {
-    return accounts.find((account) => account.accountNumber = accountNumber);
+    return this.accounts.find(
+      (account) => account.accountNumber === accountNumber,
+      // use strictly equal not loose
+    );
   }
 }
 
 // TEST YOUR CODE HERE
-
 
 // DO NOT REMOVE
 module.exports = { BankAccount, Bank };
